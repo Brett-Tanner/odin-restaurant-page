ad1895a3c4156150e43afc50ca13ebc8fa0313c8
Brett Tanner <bretttanner171@gmail.com>
2023-07-31 23:04:16 +0900

    Enable Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,22 @@ module.exports = {
   devServer: {
     static: "./dist",
   },
-  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
