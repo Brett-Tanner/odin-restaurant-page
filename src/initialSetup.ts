855c88ddefc9576763c55ece4827ac096844182c
Brett Tanner <bretttanner171@gmail.com>
2023-08-05 13:09:59 +0900

    Dynamically generate gallery

--- src/initialSetup.ts
+++ src/initialSetup.ts
@@ -15,7 +15,6 @@ import Yoghurt from "./images/yoghurt.jpg";
 import "./style.css";
 
 const main = document.getElementById("content");
-const gallery = document.getElementById("gallery");
 const foodPics = [
   Beans,
   Cake,
@@ -29,8 +28,6 @@ const foodPics = [
   Vika,
   Yoghurt,
 ];
-
-type heading = "h1" | "h2" | "h3" | "h4" | "h5";
 
 function addBlurb(text: string) {
   const blurb = document.createElement("p");
@@ -67,51 +64,24 @@ function addImage(path: string, rows: HTMLCollection | undefined) {
       throw new Error("Row missing");
     }
     rows[designatedRow].appendChild(div);
-  } else {
-    gallery?.appendChild(div);
   }
 }
 
-function addNav() {
-  const nav = document.createElement("nav");
-  nav.classList.add("flex", "flex-col", "gap-4", "self-start", "grow");
-  const menu = document.createElement("a");
-  menu.innerText = "Menu";
-  menu.id = "menuLink";
-  const contact = document.createElement("a");
-  contact.innerText = "Contact";
-  contact.id = "contactLink";
-  [menu, contact].forEach((link) => {
-    link.href = "#";
-    link.classList.add(
-      "font-heading",
-      "text-amber-700",
-      "text-2xl",
-      "p-4",
-      "hover:text-amber-950",
-      "hover:bg-amber-700",
-      "active:bg-amber-800",
-      "transition-all",
-      "duration-300"
-    );
-  });
-  nav.append(menu, contact);
-  main?.prepend(nav);
-}
-
-function addRows(picArray: any[]) {
+function createGallery(picArray: any[]) {
+  const gallery = document.createElement("div");
+  gallery.classList.add("grid", "grid-cols-2", "md:grid-cols-3", "gap-3");
   const numRows = Math.ceil(picArray.length / 4);
   for (let index = 0; index < numRows; index++) {
     const div = document.createElement("div");
     div.classList.add("grid", "gap-3");
-    gallery?.appendChild(div);
+    gallery.appendChild(div);
   }
-  return gallery?.children;
+  main?.appendChild(gallery);
+  return gallery.children;
 }
 
 export function generatePage() {
-  addNav();
-  const rows = addRows(foodPics);
+  const rows = createGallery(foodPics);
   foodPics.forEach((pic) => {
     addImage(pic, rows);
   });
